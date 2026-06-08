299bfecba31357e4be6d25929ae2bac190996779
Cristopher <cris_ldu96@yahoo.es>
2026-06-07 22:56:02 -0500

    chore(gsap): registrar useGSAP en GsapProvider

--- src/components/cosmic/GsapProvider.tsx
+++ src/components/cosmic/GsapProvider.tsx
@@ -3,13 +3,14 @@
 import { useEffect } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
 
 let registered = false;
 
 export default function GsapProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     if (!registered) {
-      gsap.registerPlugin(ScrollTrigger);
+      gsap.registerPlugin(useGSAP, ScrollTrigger);
       registered = true;
     }
     return () => {
